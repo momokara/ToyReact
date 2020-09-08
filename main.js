7a8d18cc9e3c93af2942b0bead8901753a8c58c5
momokara <1125076@qq.com>
2020-09-09 00:13:05 +0800

    添加main 入口

--- main.js
+++ main.js
@@ -1,4 +1,31 @@
-for (const iterator of [ 1, 2, 3 ]) {
-  console.log(iterator);
+function createElement(tagName, attrs, ...children) {
+  let e = document.createElement(tagName);
+  for (const attr in attrs) {
+    e.setAttribute(attr, attrs[attr]);
+  }
+
+  for (const child of children) {
+    if (typeof child === 'string') {
+      child = document.createTextNode(child);
+      e.appendChild(child);
+    }
+    e.appendChild(child);
+  }
+  return e;
 }
-let a = <div>123</div>
+window.a = (
+  <div id="main-div" class="show-num">
+    <div>父节点</div>
+    <div class="text-child">
+      子节点
+      <div class="cc">孙节点</div>
+    </div>
+    <div>父节点</div>
+    <div class="text-child">
+      子节点
+      <div>孙节点</div>
+    </div>
+  </div>
+);
+
+document.body.appendChild(window.a);
