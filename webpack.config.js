70fa62744dc5272e854dd1c778f48b106a1c2316
momokara <1125076@qq.com>
2020-09-09 00:12:57 +0800

    添加jsx插件

--- webpack.config.js
+++ webpack.config.js
@@ -16,7 +16,7 @@ module.exports = {
           loader: 'babel-loader',
           options: {
             presets: [ '@babel/preset-env' ],
-            plugins: [ [ '@babel/plugin-transform-react-jsx', { pragma: 'creatElement' } ] ]
+            plugins: [ [ '@babel/plugin-transform-react-jsx', { pragma: 'createElement' } ] ]
           }
         }
       }
